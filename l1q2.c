85af555adb71c8bd9cb21fc1b66c339f372e7572
tanay1910 <74229592+tanay1910@users.noreply.github.com>
2024-01-09 16:02:16 +0530

    Update l1q2.c
    lab work

--- l1q2.c
+++ l1q2.c
@@ -1,55 +1,105 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Graph{
-    int vertices;
+struct Graph
+{
+	int vertices;
 
-    int **table;
+	int **table;
+	int directed;
 };
 
-void addEdge(struct Graph *g1, int v1, int v2)
-{
-    g1->table[v1][v2]=1;
+struct Graph* initializeGraph(struct Graph* g1, int v, int ch)
+{	
+
+	struct Graph* temp;
+	temp = (struct Graph*)malloc(sizeof(struct Graph*));
+	temp->vertices=v;
+	temp->table=(int**)malloc(sizeof(struct Graph));
+	for(int i=0;i<v;i++)
+	{
+		temp->table[i]=(int*)malloc(v*sizeof(int));
+		for(int j=0;j<v;j++)
+		{
+			temp->table[i][j]=0;
+		}
+
+	}
+	temp->directed=ch;
+
+	return temp;
 }
 
-void printAdjMatrix(struct Graph* g1)
+void addEdge(struct Graph* g1, int v1, int v2)
 {
-    for(int i=0;i<g1->vertices;i++)
-    {
-        for(int j=0;j<g1->vertices;j++)
-        {
-            printf("%d ", g1->table[i][j]);
-        }
-        printf("\n");
-    }
+	g1->table[v1][v2]=1;
+	if(!g1->directed)
+		g1->table[v2][v1]=1;
+
 }
+
+void dispAdjMat(struct Graph* g1)
+{
+	printf("Adjacency Matrix: \n");
+	for(int i=0;i<g1->vertices;i++)
+	{
+		for(int j=0;j<g1->vertices;j++)
+		{
+			printf("%d ",g1->table[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void dispAdjList(struct Graph* g1)
+{
+	printf("Adjacency List: \n");
+	for(int i=0;i<g1->vertices;i++)
+	{
+		for(int j=0;j<g1->vertices;j++)
+		{
+			if(g1->table[i][j]==1)
+			{
+				printf("%d -> %d  ",i,j);
+			}
+		}
+		printf("\n");
+	}
+}
+
+
 
 void main()
 {
-    struct Graph *g1;
-    g1 = (struct Graph*)malloc(sizeof(struct Graph));
-    
-    int v;
-    printf("Enter no of vertices: ");
-    scanf("%d",&v);
 
-    g1->vertices=v;
+	printf("Select directed/undirected(1/0): ");
+	int ch;
+	scanf("%d",&ch);
 
-    g1->table=(int **)malloc(sizeof(int *));
-    for(int i=0;i<v;i++)
-    {
-        g1->table[i]=(int*)malloc(sizeof(int));
-        for(int j=0;j<v;j++)
-        {
-            g1->table[i][j]=0;
-        }
-    }
+	int v;
+	printf("Enter the number of vertices: ");
+	scanf("%d", &v);
 
-    
+	struct Graph* g1;
+	g1 = initializeGraph(g1,v,ch);
 
-    addEdge(g1,0,1);
+	int edg;
+	printf("Enter no of edges: ");
+	scanf("%d",&edg);
 
-    printAdjMatrix(g1);
+	for(int i=0;i<edg;i++)
+	{
+		printf("enter edge %d vertices: ", i+1);
+		int v1,v2;
+		printf("Enter vertices: ");
+		scanf("%d %d",&v1,&v2);
+		addEdge(g1,v1,v2);
+
+	}
+
+	
+	dispAdjMat(g1);
+	dispAdjList(g1);
 
 
 }
